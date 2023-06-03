d54253ebabccde6872e7953b6815d5dac8eaa290
Dzenis H <dzenis.hankusic@gmail.com>
2023-06-03 05:37:10 +0200

    ...

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const db = require("./config/database");
 mongoose.Promise = global.Promise;
 
 // Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useMongoClient: true })
+mongoose.connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useMongoClient: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
